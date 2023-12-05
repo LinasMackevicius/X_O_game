4a637f63e8acac8c3762e520b46a900f7ed78886
linasmackevicius <linasmac15@gmail.com>
2023-12-05 11:54:08 +0200

    added tests file

--- test_game.py
+++ test_game.py
@@ -1,18 +1,25 @@
-import unittest
-import tkinter as tk
+from unittest import TestCase
 
-import game
-
-
-class TestCreateMenu(unittest.TestCase):
-    def test__create_menu(self):
-        root = tk.Tk()
-        app = (root)
-        app._create_menu()
-
-        self.assertIsNotNone(app._create_menu())
+from game import TicTacToeGame, Move
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestTicTacToeGame(TestCase):
+    def test__setup_board(self):
 
+        your_instance = TicTacToeGame()  # Replace with the actual class name
+        expected_moves = [
+            [Move(row, col) for col in range(your_instance.board_size)]
+            for row in range(your_instance.board_size)
+        ]
+
+        # Act
+        your_instance._setup_board()
+
+        # Assert
+        self.assertEqual(your_instance._current_moves, expected_moves)
+
+    def test__get_winning_combos(self):
+        self.fail()
+
+    def test_toggle_player(self):
+        self.fail()
